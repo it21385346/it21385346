d46d390dc7c8418511b0228685b6aece61e5c774
it21385346 <101857435+it21385346@users.noreply.github.com>
2023-12-28 20:49:49 +0530

    Add files via upload

---
+++ Project css1/javascript1.js
@@ -0,0 +1,131 @@
+/*
+var no1=10;
+var name="arthi";
+
+//latter,numbers
+
+var h1="Headign1";
+var H1="Heading2";
+
+//array
+var animal=[23,45,4567,"tiger","lion","rabbit"];
+//animal[6]="hi";
+animal.push("hi");
+
+//alert(animal.reverse());
+
+//console
+//console.log("HII");
+
+loops
+for(var i=10;i<15;i++){
+	console.log(i)
+
+}
+
+var i1=10;
+while(i1<18)
+{
+	console.log(i1);
+	i1++;
+}
+
+
+if(1==7){
+	console.log("True")
+}
+else
+{
+	console.log("Folse")
+}
+
+
+
+var color="greed";
+switch(color){
+case"red":console.log("i like f");
+	break;
+case"blue":console.log("i like blue");
+	break;
+default:console.log("not clor");
+	break;
+}
+
+
+var preson={
+	fname:"tamil",
+	lname:"art",
+	age:22,
+	children:["raj","ram"],
+
+	address:{
+		street:3,
+		city:"jaffna",
+		country:"srilanka",
+	},
+}
+console.log(preson.children);
+
+
+
+
+var animal=new Object();
+animal.name1="tigger";
+animal.type1="killer";
+animal.color1="orange";
+
+animal.name2="lion";
+animal.type2="soft";
+animal.color2="red";
+
+function animal5(name5,type5,color5){
+	this.name5=name5;
+	this.type5=type5;
+	this.color5=color5;
+
+}
+var tiger1=new animal5("tiger","killer","ornge")
+
+var rabbit1=new animal5("rabbitttt","  "," white")
+
+console.log(tiger1);
+*/
+var username="arthigan";
+var password=123;
+console.log(username);
+console.log(password);
+
+function Welcome(username) {
+	// body...
+	console.log("Hi "++" Welcome")
+}
+Welcome("art")
+
+var personName="raj"
+var personAge=21
+var personCity="Chennai"
+
+function Welcome(){
+	console.log("Welcome to my site ")
+
+}
+var person={
+	name:"raj",
+	age:32,
+	city:"Chennai"
+}
+
+// arrays
+
+var bikeColor=['blue','orange','yellow']
+var bikeModel=[346,345,444]
+var bike=[ {color:'blue',model:346},{
+	color:'yellow',model:345}]
+
+bikeColor.push('brown')
+bikeModel.sort()                   //order or (reverse())
+console.log(bike)
+
+
+
+
